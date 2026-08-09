2e389ae95667beeadda53ec9dc09d6e1b8bbeb0e
bagi <alisson.alves1628@gmail.com>
2026-08-09 19:32:26 -0300

    fix: corrigir links de navegacao en, mapeamento de idiomas e redirecionamento relativo de dominio

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bagi-sh.com',
+  site: 'https://bagi-sh.github.io',
   i18n: {
     defaultLocale: 'pt',
     locales: ['pt', 'en'],
