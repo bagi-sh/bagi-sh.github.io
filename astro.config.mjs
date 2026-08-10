2946ff5d411c9082894cd2c26a157d3cfb437023
bagi <alisson.alves1628@gmail.com>
2026-08-10 00:36:19 -0300

    feat: new menu and post dynamics. fix: redirect website root

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,33 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bagi-sh.github.io',
+  site: "https://bagi-sh.github.io",
   i18n: {
-    defaultLocale: 'pt',
-    locales: ['pt', 'en'],
+    defaultLocale: "pt",
+    locales: ["pt", "en"],
     routing: {
       prefixDefaultLocale: true,
-      redirectToDefaultLocale: false
-    }
+      redirectToDefaultLocale: false,
+    },
   },
   markdown: {
     shikiConfig: {
-      theme: 'github-dark',
-      langs: ['c', 'cpp', 'bash', 'shell', 'json', 'markdown', 'typescript', 'javascript', 'html', 'css'],
-      wrap: true
-    }
-  }
+      theme: "github-dark",
+      langs: [
+        "c",
+        "cpp",
+        "bash",
+        "shell",
+        "json",
+        "markdown",
+        "typescript",
+        "javascript",
+        "html",
+        "css",
+        "java",
+      ],
+      wrap: true,
+    },
+  },
 });
